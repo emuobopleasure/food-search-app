a340770d1ff93820e4e544d252693a09d67bdb26
Davis06 <davis06@outlook.com>
2022-04-15 10:01:39 +0100

    created redirects folder

--- src/pages/SingleMeal.js
+++ src/pages/SingleMeal.js
@@ -21,42 +21,41 @@ const SingleMeal = () => {
         setWish((prevWish) => !prevWish)
     }
 
-    
+    const mealDetails = async () => {
+        try {
+            const res = await fetch(`${url}${id}`)
+            const foodDetails = await res.json()
+            console.log("rrr",foodDetails)
+            if (foodDetails.meals) {
+                setFoods({
+                    ...foods, 
+                    name: foodDetails.meals[0].strMeal,
+                    image: foodDetails.meals[0].strMealThumb,
+                    country: foodDetails.meals[0].strArea,
+                    category: foodDetails.meals[0].strCategory, 
+                    ingredient1: foodDetails.meals[0].strIngredient1, 
+                    ingredient2: foodDetails.meals[0].strIngredient2,
+                    ingredient3: foodDetails.meals[0].strIngredient3,
+                    ingredient4: foodDetails.meals[0].strIngredient4,
+                    ingredient5: foodDetails.meals[0].strIngredient5
+
+                })
+            }else {
+                setFoods(null)
+                // console.log('null')
+            }
+            setLoading(false)
+        }catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
     // console.log(foods.country)
     
     useEffect(()=> {
         setLoading(true)
-        const mealDetails = async () => {
-            try {
-                const res = await fetch(`${url}${id}`)
-                const foodDetails = await res.json()
-                console.log("rrr",foodDetails)
-                if (foodDetails.meals) {
-                    setFoods({
-                        ...foods, 
-                        name: foodDetails.meals[0].strMeal,
-                        image: foodDetails.meals[0].strMealThumb,
-                        country: foodDetails.meals[0].strArea,
-                        category: foodDetails.meals[0].strCategory, 
-                        ingredient1: foodDetails.meals[0].strIngredient1, 
-                        ingredient2: foodDetails.meals[0].strIngredient2,
-                        ingredient3: foodDetails.meals[0].strIngredient3,
-                        ingredient4: foodDetails.meals[0].strIngredient4,
-                        ingredient5: foodDetails.meals[0].strIngredient5
-    
-                    })
-                }else {
-                    setFoods(null)
-                    // console.log('null')
-                }
-                setLoading(false)
-            }catch (error) {
-                console.log(error)
-                setLoading(false)
-            }
-        }
         mealDetails()
-    }, [id, foods])
+    }, [id])
     
     if (loading) {
        return <Loading/>
